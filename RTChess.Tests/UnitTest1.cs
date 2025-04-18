d3161be149e424981b229b1d753785359fe1dd55
Kenyon Olsen <kenyon.olsen@students.snow.edu>
2025-04-18 08:40:31 -0600

    Tests for pieces created

--- RTChess.Tests/UnitTest1.cs
+++ RTChess.Tests/UnitTest1.cs
@@ -3,8 +3,14 @@
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void RookCreated()
     {
+        Assert.That(Board.WRook1 != null && Board.WRook2 != null && Board.BRook1 != null && Board.BRook2 != null);
+    }
 
+    [Fact]
+    public void BishopCreated()
+    {
+        Assert.That(Board.WBishop1 != null && Board.WBishop2 != null && Board.BBishop1 != null && Board.BBishop2 != null);
     }
 }
