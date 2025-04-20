024827fcd19b2b47a4ebcf6c51c5aaf134656eb1
KenyonOlsen <kenyon.olsen@students.snow.edu>
2025-04-19 23:48:43 -0600

    Update Game.cs

--- RTChess.Logic/Game.cs
+++ RTChess.Logic/Game.cs
@@ -1,17 +1,12 @@
 namespace RTChess.Logic;
 public class Game
 {
-    public Player P1 { get; set; }
-    public Player P2 { get; set; }
+    public static Player P1 { get; set; } = new Player(true);
+    public static Player P2 { get; set; } = new Player(false);
 
-    public Game(Player p1, Player p2)
-    {
-        P1 = p1;
-        P2 = p2;
-    }
-
-    public Game()
-    {
-        
-    }
+    //public Game(Player p1, Player p2)
+    //{
+    //    P1 = p1;
+    //    P2 = p2;
+    //}
 }
